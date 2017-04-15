57478de4154db14e73b3b9770f2f32ecdde8f526
BHIMNATH <you@example.com>
2017-04-15 23:29:47 +0530

    second commit

---
+++ FileLister/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace FileLister
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = Directory.GetFiles(filename.Text);
+            //string[] dir = Directory.GetDirectories();
+            foreach (string f in files)//string d in dir
+            {
+                filelist.AppendText(f.ToString()+"\n");
+            }
+        }
+
+        private void filename_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
